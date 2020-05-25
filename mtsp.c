2363e50b7c782eb30e4cae45cc6d827b1aeb109b
Park Minjoo <21800268@handong.edu>
2020-05-25 21:31:41 +0900

    Input

--- mtsp.c
+++ mtsp.c
@@ -4,7 +4,19 @@
 #include <pthread.h>
 #include <sched.h>
 
+//Input
 int main(char *argv[], int argc) /*./mstp gr17.tsp 8*/
 {
+	//argv is a filr name of a TSP instance data 
+	//argc is the initial number of comsumer thread
 	
+	// if the initial number is larger than 8
+	if (argc > 8) printf("the limit is 8, please try again\n"); 		
+	//initial number is smaller than 9
+	else {
+		
+	}
 }
+
+//Output
+
